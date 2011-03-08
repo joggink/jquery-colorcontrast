872499e20cf66d544f6b1cb588030efa281b23c4
joggink <hello@joggink.com>
2011-03-08 03:35:21 +0100

    expanding the plugin background color detection adding first real example

--- jquery.colorcontrast.js
+++ jquery.colorcontrast.js
@@ -13,15 +13,51 @@
 			Initialises the color contrast
 
 			Parameters:
-				jQuery object(s) - {object}
+				jQuery object - {object}
 
 			Example
 				> // initialise new color contrast calculator
 				> $('body').colorcontrast();
 		  
 		*/
-		init : function(o) {
-
+		init : function() {
+			// check if we have a background image, if not, use the backgroundcolor
+			$(this).colorcontrast('bgColor');
+			return this;
+		},
+		bgColor : function() {
+			var t = $(this);
+			t.removeClass('dark light');
+			t.addClass($(this).colorcontrast('calculateYIQ', t.css('background-color')));
+		},
+		calculateYIQ : function(color){
+			var r = 0, g = 0, b = 0, a = 1, yiq = 0;
+			if (/rgba/.test(color)){
+				color = color.replace('rgba(', '').replace(')', '').split(/,/);
+				r = color[0];
+				g = color[1];
+				b = color[2];
+				a = color[3];
+			}else if (/rgb/.test(color)){
+				color = color.replace('rgb(', '').replace(')', '').split(/,/);
+				r = color[0];
+				g = color[1];
+				b = color[2];
+			}else if(/#/.test(color)){
+				color = color.replace('#', '');
+				if (color.length == 3){
+					var _t = '';
+					_t += color[0] + color[0];
+					_t += color[1] + color[1];
+					_t += color[2] + color[2];
+					color = _t;
+				}
+				r = parseInt(color.substr(0,2),16);
+				g = parseInt(color.substr(2,2),16);
+				b = parseInt(color.substr(4,2),16);
+			}
+			yiq = ((r*299)+(g*587)+(b*114))/1000;
+			return (yiq >= 128) ? 'light' : 'dark';
 		}
 	};
 	$.fn.colorcontrast = function(method){
